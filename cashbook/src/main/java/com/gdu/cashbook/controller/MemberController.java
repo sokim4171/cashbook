9a47a32e9140a062a9fd26868e730266a15834d9
GD7 <GD7@DESKTOP-2JEKVA2>
2020-05-13 12:06:49 +0900

    [김서영] 로그인 기능 구현 (loginMember select 매퍼,서비스,컨트롤러) 로그인 세션처리 로그아웃 기능

--- cashbook/src/main/java/com/gdu/cashbook/controller/MemberController.java
+++ cashbook/src/main/java/com/gdu/cashbook/controller/MemberController.java
@@ -1,11 +1,14 @@
 package com.gdu.cashbook.controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.gdu.cashbook.service.MemberService;
+import com.gdu.cashbook.vo.LoginMember;
 import com.gdu.cashbook.vo.Member;
 
 @Controller
@@ -14,13 +17,37 @@ public class MemberController {
 	@Autowired
 	private MemberService memberService;
 	
+	@GetMapping("/login") 
+	public String Login() {
+		return "Login";
+	}
+	
+	@PostMapping("/login")
+	public String Login(LoginMember loginMember,HttpSession session) {
+		System.out.println(loginMember.toString());
+		LoginMember returnLoginMember=memberService.login(loginMember);
+		System.out.println(returnLoginMember);
+		if(returnLoginMember==null) { //login fail
+			return "redirect:/login";
+		} else { //login success
+			session.setAttribute("loginMember", returnLoginMember);
+			return "redirect:/index";
+		}
+	}
+	
+	@GetMapping("/logout")
+	public String Logout(HttpSession session) {
+		session.invalidate();
+		return "redirect:/index";
+	}
+	
 	@GetMapping("/addMember")
 	public String addMember() {
 		return "addMember";
 	}
 	
 	@PostMapping("/addMember")
-	public String addMember(Member member) { //command 객체,도메인 객체
+	public String addMember(Member member) { 
 		System.out.println(member.toString());
 		memberService.addMember(member);
 		return "redirect:/addMember";
--- cashbook/src/main/java/com/gdu/cashbook/mapper/MemberMapper.java
+++ cashbook/src/main/java/com/gdu/cashbook/mapper/MemberMapper.java
@@ -2,9 +2,11 @@ package com.gdu.cashbook.mapper;
 
 import org.apache.ibatis.annotations.Mapper;
 
+import com.gdu.cashbook.vo.LoginMember;
 import com.gdu.cashbook.vo.Member;
 
 @Mapper
 public interface MemberMapper {
+	public LoginMember selectLoginMember(LoginMember loginMember);
 	public int insertMember(Member member);
 }
