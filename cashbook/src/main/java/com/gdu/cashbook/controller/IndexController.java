b8ac53063d0a6593bc5771ec3f7381859a74771e
GD7 <GD7@DESKTOP-2JEKVA2>
2020-05-13 10:15:34 +0900

    [김서영] 매퍼 수정 및  addMember 부트스트랩 적용

--- cashbook/src/main/java/com/gdu/cashbook/controller/IndexController.java
+++ cashbook/src/main/java/com/gdu/cashbook/controller/IndexController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class IndexController {
-	@GetMapping("/index")
+	@GetMapping({"/","/index"})
 	public String index() {
 		return "index";
 	}
